df5a73b8f59f8e5dd5f007bf24f71bbbda9f31da
KazuOfficial <kazuofficial.contact@gmail.com>
2020-08-19 15:53:26 +0200

    Init

---
+++ ChristmasCountdown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChristmasCountdown
+//
+//  Created by Jakub on 8/19/20.
+//  Copyright © 2020 Kazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
